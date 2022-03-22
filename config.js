270c241cc245703cf83e2c84f897ebe907b7bbde
ntaautofot4 <102160168+ntaautofot4@users.noreply.github.com>
2022-03-22 20:31:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
-    titleWeb: "sent",
+    titleWeb: "demo",
     introTitle: 'Lê Thị Thùy',
-    introDesc: `Phiếu khảo sát tăng lương định kì`,
+    introDesc: `Khảo sát tăng lương công, nhân viên trong công ty`,
     btnIntro: '^^Đồng ý^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên $',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy $ ',
-    btnYes: 'Thích lắm, vào trả lời câu hỏi <33',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: Bạn chấp nhận lời đề nghị lên lương của chúng tôi chứ!',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Tôi thấy mức lương của mình phù hợp rồi! cảm ơn công ty đã quan tâm',
-    mess: 'Công ty biết mà 🥰. cảm ơn bạn nhiều đã cống hiến cho công ty 😘😘',
-    messDesc: '17/4 - chúng mình đi nhậu nha.',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://fb.com' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
