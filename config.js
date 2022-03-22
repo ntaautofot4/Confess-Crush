aeda8549b271a27a216419058db7d35d920496ce
ntaautofot4 <102160168+ntaautofot4@users.noreply.github.com>
2022-03-22 20:33:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,11 @@
 const CONFIG = {
-    titleWeb: "demo",
-    introTitle: 'Lê Thị Thùy',
-    introDesc: `Khảo sát tăng lương công, nhân viên trong công ty`,
-    btnIntro: '^^Đồng ý^^',
+    titleWeb: "DEmo",
+    introTitle: 'A',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
